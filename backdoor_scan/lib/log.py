103308a3c42d7e389d281acfc644c10d939d532e
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-04-10 14:00:56 +0800

    first modification

--- backdoor_scan/lib/log.py
+++ backdoor_scan/lib/log.py
@@ -1,10 +1,10 @@
 import logging
 
 # logger
-formatter = logging.Formatter('[%(levelname)s] %(message)s')
+formatter = logging.Formatter('[📅 %(asctime)s][💼%(levelname)s] 📝 %(message)s')
 handler = logging.StreamHandler()
 handler.setFormatter(formatter)
 #logger = logging.getLogger("veinmind-sensitive")
 logger = logging.getLogger("xApp-sensitive")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.INFO)  ## It just output basic info
 logger.addHandler(handler)
