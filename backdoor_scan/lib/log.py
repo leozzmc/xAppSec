6b5781f94aa718a1514ad29c00ca66fe64b602d7
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-04-10 14:29:15 +0800

    modify log.py for logging format

--- backdoor_scan/lib/log.py
+++ backdoor_scan/lib/log.py
@@ -1,7 +1,7 @@
 import logging
 
 # logger
-formatter = logging.Formatter('[📅 %(asctime)s][💼%(levelname)s] 📝 %(message)s')
+formatter = logging.Formatter( '😀'+'%(asctime)s [%(levelname)s]  %(message)s')
 handler = logging.StreamHandler()
 handler.setFormatter(formatter)
 #logger = logging.getLogger("veinmind-sensitive")
