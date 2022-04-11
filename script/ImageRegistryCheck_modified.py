6af920da67804b98dabde9318da3b433a1667a7d
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-04-11 21:43:53 +0800

    Modified the ImageRegistryCheck_modified.py

--- script/ImageRegistryCheck_modified.py
+++ script/ImageRegistryCheck_modified.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 import json
 import click
+import time as timep
 from lib import tools
 from veinmind import *
 
-
+start = 0
 Registry_List = []
+Decision = []
+
 
 # The registry whitelist is only "nexus3.o-ran-sc.org:10002" for now.
 While_List = ["nexus3.o-ran-sc.org:10002"]
 
 @command.group()
 @click.option('--format', default="stdout", help="output format e.g. stdout/json")
-def cli(format ):
+def cli(format):
+    global start
+    start = timep.time()
     pass
 
 # @cli.command()
@@ -31,23 +36,39 @@ def cli(format ):
 #             print (f"\n❎ the image registry: \"{i}\" of xApp is invalid!")
 
 @cli.image_command()
-def registry_check():
-    pass
+def registry_check(image):
+    """check the registry of image within xApp descriptor files"""
+    global Registry_List
+    global Decision
+    Registry_List.append(image.id())
+    if len(image.reporefs()) > 0:
+        log.info("start scan: " + image.reporefs()[0])
+    else:
+        log.info("start scan: " + image.id())
+    
+    # the registry in Registry_List contain 'True', it means the registry != "nexus3.o-ran-sc.org:10002"
+    for i in Registry_List:
+        if i != "nexus3.o-ran-sc.org:10002":
+            Decision.append(True)
+        else:
+            Decision.append(False)
+
 
 
 
 @cli.resultcallback()
 def callback(Registry_List, format):
+    spend_time = timep.time() - start
+
     if format == "stdout":
         print("# ================================================================================================= #")
-        tools.tab_print(" >> \033[48;5;234m\033[38;5;202mScan Image Total:\033[0;0m " + str(len(image_ids)))
+        tools.tab_print(" >> \033[48;5;234m\033[38;5;202mScan Image Total:\033[0;0m " + str(len(Registry_List)))
         tools.tab_print(" >> \033[48;5;234m\033[38;5;202mSpend Time:\033[0;0m " + spend_time.__str__() + "s")
-        tools.tab_print(" >> \033[48;5;234m\033[38;5;202mBackdoor Total:\033[0;0m " + str(len(results)))
-        for r in results:
-            print("+---------------------------------------------------------------------------------------------------+")
-            tools.tab_print("ImageName: " + r.image_ref)
-            tools.tab_print("Backdoor File Path: " + r.filepath)
-            tools.tab_print("Description: " + r.description)
+        for r in range(0,len(Decision)):
+            if Decision[r] == 'True':
+                print("+---------------------------------------------------------------------------------------------------+")
+                tools.tab_print("ImageName: " + Registry_List[r] )
+                tools.tab_print("Descriptions: " + "the image registry of this image is invalid")
         print("+---------------------------------------------------------------------------------------------------+")
         print("# ================================================================================================= #")
         pass
