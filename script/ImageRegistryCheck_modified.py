06d3baeb5245f89e01465b34a0a3c17e0ddeeb21
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-04-13 12:34:23 +0800

    Change color

--- script/ImageRegistryCheck_modified.py
+++ script/ImageRegistryCheck_modified.py
@@ -53,9 +53,9 @@ def callback(result, format ):
             tools.tab_print("RegistryName: " + str(registry_List[r]),expandNum=100)
             print("+---------------------------------------------------------------------------------------------------+") 
             if decision[r] == True:
-                tools.tab_print("Descriptions: " + "the image registry of this image is \033[48;5;197m INVALID \033[0;0m",expandNum=117)
+                tools.tab_print("Descriptions: " + "the image registry of this image is \033[48;5;160m INVALID \033[0;0m",expandNum=117)
             else:
-                tools.tab_print("Descriptions: " + "the image registry of this image is \033[48;5;46m VALID \033[0;0m",expandNum=116)
+                tools.tab_print("Descriptions: " + "the image registry of this image is \033[48;5;30m VALID \033[0;0m",expandNum=116)
         print("+---------------------------------------------------------------------------------------------------+")
         print("# ================================================================================================= #")
         pass
