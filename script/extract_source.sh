a0b032f449573157af06b8b61eb2f28831bea55d
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-04-19 20:49:59 +0800

    Add "extract_source file"

---
+++ script/extract_source.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Step 1 -  docker inspcet <container_name>  
+### 1.1   Get  "WorkingDir"
+### 1.2   Get "CMD" 
+### 1.3.  Judge the source code is .c, .py, .go or bash file
+
+## Step 2 -  docker cp <container_name>:</path/to/the/file> </local/system/path>
+
+## Step 3  - Return source code path
