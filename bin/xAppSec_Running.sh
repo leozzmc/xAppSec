6ac4588b5615309065fa4c9f8cfa58336f85a155
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-05-28 22:07:37 +0800

    Add xAppSec_Running

---
+++ bin/xAppSec_Running.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# '''This scipt is cmd tool for onboarding xApp'''
+# '''Then do the Image Security Check'''
+
+
+
+#  Get the xApp descriptor file paths
+
+#  Install dms_cli(xapp_onboarder)
+
+#  Onboard the xApp
+
+#  ImageRegistryCheck.py
+
+#  Backdoor_Scan.py
+
+#  ImageHistory_Scan.py
+
+#  Ask if want to install the xApp
+
+#  Install the xApp (If yes)
+
+#  Check if xApp is in "Running" State
+
+#  Return the Kibana Service Address
