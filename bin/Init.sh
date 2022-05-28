8a20fddb5975a64071bae8cd7137c4465d8a2ef9
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-05-28 23:10:23 +0800

    Add Ca-Certificates

--- bin/Init.sh
+++ bin/Init.sh
@@ -3,6 +3,7 @@
 
 #  Install VeinMind SDK
 echo ">>> Downloading VeinMind SDK...."
+sudo apt-get install -y ca-certificates
 echo 'deb [trusted=yes] https://download.veinmind.tech/libveinmind/apt/ ./' | sudo tee /etc/apt/sources.list.d/libveinmind.list
 sudo apt-get update
 sudo apt-get install -y libveinmind-dev
