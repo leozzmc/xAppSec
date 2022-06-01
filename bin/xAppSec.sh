65c03af21ef5bb90289c688153ffb730ecc2901c
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-06-01 16:21:23 +0800

    Rename

--- bin/xAppSec.sh
+++ bin/xAppSec.sh
@@ -1,105 +1,114 @@
 #!/bin/bash
+# '''This scipt is cmd tool for onboarding xApp'''
+# '''Then do the Image Security Check'''
+
+ARG_NUM=1
 
 
-#### 0. Store the parameters from user
 
-read -p "😎 Please provide the path to your xApp descriptor configuration file: " CONFIG_JSON
-read -p "😎 Please provide the path to your xApp descriptor schema file: " SCHEMA_JSON
-echo  -e "\n📂 xApp Descriptor Config.json Path:" ${CONFIG_JSON} 
-echo  -e "📂 xApp Descriptor Schema.json Path:" ${SCHEMA_JSON}
+if [ $# -ne $ARG_NUM ]; then
+   echo "Wrong number of parameters!"
+   cat ../manual.txt
+   exit 1
+fi
 
-#### 1. Install dms_cli tool
-echo "🕓   Installaling the dms_cli tool -------------------------"
+if [ $1 != "-i" ] && [ $1 != "-n" ] && [ $1 != "-k" ] && [ $1 != "-h" ];then
+  echo "Wrong parameter!"
+  cat ../manual.txt
+  exit 1
+fi 
+
+
+if [ $1 ==  "-i" ]; then
+   chmod +x Init.sh
+   ./Init.sh
+   exit
+elif [ $1 == "-n" ]; then
+   echo "normal mode."
+elif [ $1 == "-k" ];then
+   echo "To Setup Kibana Index Pattern."
+   chmod +x kibana.sh
+   ./kibana.sh
+   exit
+else
+  cat ../manual.txt
+  exit  
+fi
+
+
+
+#  Get the xApp descriptor file paths
+read -p "Please provide the path to your xApp descriptor configuration file: " CONFIG_JSON
+read -p "Please provide the path to your xApp descriptor schema file: " SCHEMA_JSON
+echo  -e "\nxApp Descriptor Config.json Path:" ${CONFIG_JSON} 
+echo  -e "xApp Descriptor Schema.json Path:" ${SCHEMA_JSON}
+
+#  Onboard the xApp
+dms_cli onboard $CONFIG_JSON $SCHEMA_JSON
 sleep 2
-
-sudo apt-get install -y python3-pip jq
-git clone "https://gerrit.o-ran-sc.org/r/ric-plt/appmgr"
-cd appmgr/xapp_orchestrater/dev/xapp_onboarder
-pip3 install ./
-sudo chmod 755 /usr/local/bin/dms_cli
-
-#### 2. Run the local helm server
-echo "🕓   Running the local helm server -------------------------"
-sleep 2
-
-docker run --rm -u 0 -it -d -p 8080:8080 \
-    -e DEBUG=1 \
-    -e STORAGE=local \
-    -e STORAGE_LOCAL_ROOTDIR=/charts \
-    -v $(pwd)/charts:/charts chartmuseum/chartmuseum:latest 
-
-export CHART_REPO_URL=http://0.0.0.0:8080
-
-#### 3. Received the xApp Config.json and Scehma.json to onboard xApp
-echo "🕓   Starting to onboard the xApp --------------------------"
-sleep 2
-
-dms_cli onboard  ${CONFIG_JSON} ${SCHEMA_JSON}
-
 curl -X GET http://localhost:8080/api/charts | jq .
 
+#  Get ImageName
+echo "+-----------------------------------------------+"
+CONTAINER_NUM=$(cat  ${CONFIG_JSON}  | jq -c ".containers" | jq length)
 
-#### 4. Call the Static Analysis Function
-echo "🧫   Perform static analysis to the xApp -------------------"
-sleep 2
-
-######  4.1 Helm Charts Analysis
-
-#  Note
-#  dms_cli install_values_yaml hwxapp 1.0.0 ricxapp --output_path=.
-#  Q1: How to get the xApp name and version number?
-#  A1: Use " cat config-file.json | jq -c '.version' " and " cat config-file.json | jq -c '.xapp_name' "
-
-XAPP_NAME=$(cat ${CONFIG_JSON} | jq -c '.xapp_name')
-XAPP_VERSION=$(cat ${CONFIG_JSON} | jq -c '.version')
-
-dms_cli install_values_yaml ${XAPP_NAME} ${XAPP_VERSION} ricxapp  --output_path=.
-
-
-######  4.2 Image Regsitry Check
-
-CONTAINER_NUM=$(cat $CONFIG_JSON | jq -c ".containers" | jq length)
-declare -a REGISTRY=()
-
-# Store the container registrys to array
 INDEX=0
 while [ $INDEX -lt  $CONTAINER_NUM ]
 do
-	REGISTRY+=($(cat $CONFIG_JSON | jq -c ".containers[$INDEX].image.registry"))
-	echo "[$INDEX] | Value: ${REGISTRY[$INDEX]}"
-	(( INDEX++ ))
+    REGISTRY=$(cat  ${CONFIG_JSON}  | jq -c ".containers[$INDEX].image.registry"| tr -d '"')
+    NAME=$(cat  ${CONFIG_JSON}  | jq -c ".containers[$INDEX].image.name"| tr -d '"')
+    TAG=$(cat  ${CONFIG_JSON}  | jq -c ".containers[$INDEX].image.tag" | tr -d '"')
+	echo $REGISTRY
+	echo $NAME
+	echo $TAG
+	docker pull "$REGISTRY/$NAME:$TAG"
+    (( INDEX++ ))
 done
 
 
-echo "✅ Complete stroing the container registries to array"
+#  ImageRegistryCheck.py
+echo ">>> Scanning Image Registry ...."
+chmod +x ~/xAppSec/Image_Security_Module/ImageRegistryCheck.py
+../Image_Security_Module/ImageRegistryCheck.py registry-check $NAME
 
-echo "📜 Comparing the Registry List with Whitlist...."
+#  Backdoor_Scan.py
+echo ">>> Scanning If Backdors Within Image...."
+chmod +x ~/xAppSec/Image_Security_Module/backdoor_scan.py
+../Image_Security_Module/backdoor_scan.py xapp-scan-images $NAME
 
-### COMPARASION=('{ echo "${REGISTRY[*]}"; echo "${WHITELIST[*]}"; } | sort | uniq -d ') #顯示重複值
-### COMPARASION=('{ echo "${REGISTRY[*]}"; echo "${WHITELIST[*]}"; } | sort | uniq -u ') #顯示非重複值
-### echo "${COMPARASION[*]}
+#  ImageHistory_Scan.py
+echo ">>> Scanning History Instructions in the Image...."
+cd ~/xAppSec/Image_Security_Module/
+chmod +x image_history.py
+../Image_Security_Module/image_history.py xapp-scan-images $NAME
+#  Ask if want to install the xApp
 
+kubectl get pods 
 
+echo "+------------------------------+"
 
+until [ "${ANSWER}" == "Y" ] || [ "${ANSWER}" == "N" ] 
+do 
+   read -p "If you want to install the xApp.(Y/N) ? " ANSWER
+done 
 
-# Compare with whitelist
-#python3 ~/xAppSec/script/ImageRegistryCheck.py  [parameter1: CONFIG_JSON] [parameter2: SCHEMA_JSON]
+if [ "${ANSWER}" != "Y" ]
+then
+  echo "Bye Bye."
+  exit
+fi 
+#  Install the xApp (If yes)
 
+XAPP_NAME=$(cat  ${CONFIG_JSON}  | jq -c ".xapp_name"| tr -d '"')
+XAPP_VERSION=$(cat  ${CONFIG_JSON}  | jq -c ".version"| tr -d '"')
+echo "Installing xApp ...."
+dms_cli install $XAPP_NAME $XAPP_VERSION ricxapp
 
+sleep 15
+#  Check if xApp is in "Running" State
+kubectl get pods -n ricxapp
 
-
-
-
-
-
-
-######  4.3 Image Vulnerabities Check
-
-
-#### 5. dms_cli tool install the xApp
-
-
-
-#### 6. Continuously check the deployment of xApp
-
-#### 7. Call the Dynamic Security Analysis function
+#  Return the Kibana Service Address
+URL=$(kubectl get svc | grep kibana-np | cut -c 29-43| tr -d " ")
+echo "Now You Can Acces the Kibana Service."
+echo "Kibana URL: http://${URL}:8080/" 
