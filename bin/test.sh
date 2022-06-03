eb0d99449646cf3e788f78fb08e3cae1e7cf3103
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-06-03 20:48:34 +0800

    POST A Search

--- bin/test.sh
+++ bin/test.sh
@@ -5,10 +5,10 @@ KIBANA_EP=$(kubectl get ep| grep kibana-np|cut -c 15-35| tr -d " ")
 
 echo "Sending API Request to Kibana service ...."
 
-curl -X POST $KIBANA_EP/api/saved_objects/index-pattern/index-pattern-id  -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d '
+curl -X POST $KIBANA_EP/api/saved_objects/search/my-search-id -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d '
 {
   "attributes": {
-    "title": "xapp-*",
+    "title": "my-search",
     "timeFieldName": "@timestamp"
   }
 }'
