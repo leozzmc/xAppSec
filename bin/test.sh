e9e74cba90fddf514eb25993b3408b706d3b36e5
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-06-06 20:58:33 +0800

    Add Script Field

--- bin/test.sh
+++ bin/test.sh
@@ -5,7 +5,7 @@ KIBANA_EP=$(kubectl get ep| grep kibana-np|cut -c 15-35| tr -d " ")
 
 echo "Sending API Request to Kibana service ...."
 
-curl -X POST $KIBANA_EP/api/saved_objects/search/my-search-id -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d '
+curl -X GET $KIBANA_EP/api/saved_objects/_find?type=index-pattern&search_fields=title&search=xapp-* -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d '
 {
   "attributes": {
     "title": "my-search",
@@ -14,3 +14,25 @@ curl -X POST $KIBANA_EP/api/saved_objects/search/my-search-id -H 'kbn-xsrf: true
 }'
 
 
+# Add Scripted Field in Kibana
+if (doc['kubernetes.namespace_name.keyword'].size() != 0 ){
+  if (doc['message.keyword'].size() != 0){
+    if (doc['msg.keyword'].size() != 0 ){
+      if (doc['message.keyword'].size() == doc['msg.keyword'].size()){
+        return doc['message.keyword'].value;
+      }
+    }
+    else if (doc['log.keyword'].size() != 0 ){
+      if (doc['message.keyword'].size() == doc['log.keyword'].size()){
+        return doc['message.keyword'].value;
+      }
+    }
+  }
+  else if (doc['log.keyword'].size() != 0){
+    if (doc['msg.keyword'].size() != 0 ){
+      if (doc['log.keyword'].size() == doc['msg.keyword'].size()){
+        return doc['log.keyword'].value;
+      }
+    }
+  }
+}
