23b082ae8d7fdb13b6811f2d7afb57b2d5b89968
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-06-06 21:15:49 +0800

    Add Script Field

--- bin/test.sh
+++ bin/test.sh
@@ -4,35 +4,62 @@
 KIBANA_EP=$(kubectl get ep| grep kibana-np|cut -c 15-35| tr -d " ")
 
 echo "Sending API Request to Kibana service ...."
+echo "Testing Script."
 
-curl -X GET $KIBANA_EP/api/saved_objects/_find?type=index-pattern&search_fields=title&search=xapp-* -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d '
+# Add Scripted Field in Kibana
+curl -X POST api/saved_objects/index_pattern/index-pattern-id/runtime_field -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d '
 {
-  "attributes": {
-    "title": "my-search",
-    "timeFieldName": "@timestamp"
+  "name": "xapp-*",
+  "TestField": {
+     "type": "string",
+     "script": {
+        "lang": "painless",
+        "source": """
+          if (doc['kubernetes.namespace_name.keyword'].size() != 0 && doc['stream.keyword'].value == "stderr" ){
+            if (doc['message.keyword'].size() != 0){
+              if (doc['msg.keyword'].size() != 0 ){
+                if (doc['message.keyword'].size() == doc['msg.keyword'].size()){
+                  return doc['message.keyword'].value;
+                }
+              }
+              else if (doc['log.keyword'].size() != 0 ){
+                if (doc['message.keyword'].size() == doc['log.keyword'].size()){
+                  return doc['message.keyword'].value;
+                }
+              }
+            }
+            else if (doc['log.keyword'].size() != 0){
+              if (doc['msg.keyword'].size() != 0 ){
+                if (doc['log.keyword'].size() == doc['msg.keyword'].size()){
+                  return doc['log.keyword'].value;
+                }
+              }
+            }
+          } 
+        """
+      }
   }
 }'
 
 
-# Add Scripted Field in Kibana
-if (doc['kubernetes.namespace_name.keyword'].size() != 0 ){
-  if (doc['message.keyword'].size() != 0){
-    if (doc['msg.keyword'].size() != 0 ){
-      if (doc['message.keyword'].size() == doc['msg.keyword'].size()){
-        return doc['message.keyword'].value;
-      }
-    }
-    else if (doc['log.keyword'].size() != 0 ){
-      if (doc['message.keyword'].size() == doc['log.keyword'].size()){
-        return doc['message.keyword'].value;
-      }
-    }
-  }
-  else if (doc['log.keyword'].size() != 0){
-    if (doc['msg.keyword'].size() != 0 ){
-      if (doc['log.keyword'].size() == doc['msg.keyword'].size()){
-        return doc['log.keyword'].value;
-      }
-    }
-  }
-}
+# if (doc['kubernetes.namespace_name.keyword'].size() != 0 && doc['stream.keyword'].value == "stderr" ){
+#   if (doc['message.keyword'].size() != 0){
+#     if (doc['msg.keyword'].size() != 0 ){
+#       if (doc['message.keyword'].size() == doc['msg.keyword'].size()){
+#         return doc['message.keyword'].value;
+#       }
+#     }
+#     else if (doc['log.keyword'].size() != 0 ){
+#       if (doc['message.keyword'].size() == doc['log.keyword'].size()){
+#         return doc['message.keyword'].value;
+#       }
+#     }
+#   }
+#   else if (doc['log.keyword'].size() != 0){
+#     if (doc['msg.keyword'].size() != 0 ){
+#       if (doc['log.keyword'].size() == doc['msg.keyword'].size()){
+#         return doc['log.keyword'].value;
+#       }
+#     }
+#   }
+# }
