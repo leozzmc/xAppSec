2839ed8687e38b0b54a221e8f32433babc4b45db
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-06-07 21:52:22 +0800

    Add TestJson

--- bin/test.sh
+++ bin/test.sh
@@ -7,39 +7,7 @@ echo "Sending API Request to Kibana service ...."
 echo "Testing Script."
 
 # Add Scripted Field in Kibana
-curl -X POST $KIBANA_EP/api/saved_objects/index_pattern/index-pattern-id/runtime_field -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d '
-{
-  "name": "xapp-*",
-  "TestField": {
-     "type": "string",
-     "script": {
-        "lang": "painless",
-        "source": """
-          if (doc['kubernetes.namespace_name.keyword'].size() != 0 && doc['stream.keyword'].value == "stderr" ){
-            if (doc['message.keyword'].size() != 0){
-              if (doc['msg.keyword'].size() != 0 ){
-                if (doc['message.keyword'].size() == doc['msg.keyword'].size()){
-                  return doc['message.keyword'].value;
-                }
-              }
-              else if (doc['log.keyword'].size() != 0 ){
-                if (doc['message.keyword'].size() == doc['log.keyword'].size()){
-                  return doc['message.keyword'].value;
-                }
-              }
-            }
-            else if (doc['log.keyword'].size() != 0){
-              if (doc['msg.keyword'].size() != 0 ){
-                if (doc['log.keyword'].size() == doc['msg.keyword'].size()){
-                  return doc['log.keyword'].value;
-                }
-              }
-            }
-          } 
-        """
-      }
-  }
-}'
+curl -X POST $KIBANA_EP/api/index_patterns/index_pattern/index-pattern-id/runtime_field -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d @test.json
 
 
 # if (doc['kubernetes.namespace_name.keyword'].size() != 0 && doc['stream.keyword'].value == "stderr" ){
