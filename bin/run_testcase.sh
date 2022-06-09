a819e2f4bc732881e342b352e4df0d3757ba13a1
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-06-09 14:33:07 +0800

    Add to run_test_case.sh

--- bin/run_testcase.sh
+++ bin/run_testcase.sh
@@ -5,5 +5,12 @@ cd ~/xAppSec/Testing/test_case/xApp_descriptor/ad
 echo ">>> Building fake ad xApp..."
 docker build -t nexus3.o-ran-sc.org:10002/fake_ad_xapp:0.0.1 .
 cd ..
-echo ">>> fake xApp descriptor config file path: "
+echo ">>> fake AD xApp descriptor config file path: "
 echo "$(pwd)/malicious_ad_config.json"
+
+cd ~/xAppSec/Testing/test_case/xApp_descriptor/qp 
+echo ">>> Building fake qp xApp..."
+docker build -t self_defined_registry:10002/fake_qp_xapp:0.0.1 .
+cd ..
+echo ">>> fake QP xApp descriptor config file path: "
+echo "$(pwd)/malicious_qp_config.json"
