474a0b35160854a3452d4ed3b227704438154db3
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-06-09 18:05:31 +0800

    Fix Registry address

--- bin/run_testcase.sh
+++ bin/run_testcase.sh
@@ -16,8 +16,8 @@ docker run -d -p 5000:5000 --name private_registry --restart always registry:lat
 
 
 echo ">>> Building fake qp xApp..."
-docker build -t localhost:5000/fake_qp_xapp:0.0.1 .
-docker push localhost:5000/fake_qp_xapp:0.0.1
+docker build -t 127.0.0.1:5000/fake_qp_xapp:0.0.1 .
+docker push 127.0.0.1:5000/fake_qp_xapp:0.0.1
 cd ..
 echo ">>> fake QP xApp descriptor config file path: "
 echo "$(pwd)/malicious_qp_config.json"
